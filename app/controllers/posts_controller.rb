73543a58e5c7ecfb124f969bc24eb01df328691e
Guilherme Zordan <guihermezordan@outlook.com>
2017-02-20 20:49:46 -0300

    create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,18 @@
 class PostsController < ApplicationController
+
   def index
-    @posts = Post.all
+    @posts = Post.all.order created_at: :desc
+  end
+
+  def create
+    if Post.create post_params
+      redirect_to posts_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit :title
   end
 end
--- db/migrate/20170214003104_create_posts.rb
+++ db/migrate/20170214003104_create_posts.rb
@@ -2,6 +2,9 @@ class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
       t.string :title
+      t.integer :funny_count
+      t.integer :smart_count
+      t.integer :negative_count
 
       t.timestamps
     end
