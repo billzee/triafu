42ce4bf994b440ebefaa4a74a1d592147b686805
Guilherme Zordan <guilhermezordan@outlook.com>
2017-04-19 18:07:02 -0300

    alertas na tela principal da aplicacao corrigido

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,11 @@
 $(function() {
-  setTimeout(function(){
-     $(".communications").slideUp(500);
-   }, 2700);
+  console.log('chamou');
+  if ($(".communications").length){
+    $(".panel-container").css("padding-top", "5.5em");
+
+    setTimeout(function(){
+      $(".communications").slideUp(100);
+      $(".panel-container").css("padding-top", "3.5em");
+    }, 3500);
+  }
 });
