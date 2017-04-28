c586b5d1d65f270319956f4839dbfbda6a243139
Guilherme Zordan <guilhermezordan@outlook.com>
2017-04-28 14:21:32 -0300

    ajustes de dependencias

--- client/app/post/PostDropzone.jsx
+++ client/app/post/PostDropzone.jsx
@@ -9,27 +9,28 @@ export default class PostDropzone extends Component {
     this.state = {preview: null, loading: null, errors: {}};
   }
 
-  async onDrop(files) {
-    try{
-      this.setState({
-        preview: {backgroundImage: "url('" + files[0].preview + "')"},
-        loading: true
+  onDrop(files) {
+    this.setState({
+      preview: {backgroundImage: "url('" + files[0].preview + "')"},
+      loading: true
+    });
+
+    PostsApi._upload_media(files[0]).then(function(response) {
+      var reader = response.body.getReader();
+      var bytesReceived = 0;
+
+      reader.read().then(function processResult(result) {
+        if (result.done) {
+          console.log("Fetch complete");
+          return;
+        }
+        console.log(result);
+        bytesReceived += result.value.length;
+        console.log(`Received ${bytesReceived} bytes of data so far, total: ${files[0].size}`);
+
+        return reader.read().then(processResult);
       });
-
-      let res = await PostsApi._upload_media(files[0]);
-      let resJson = await res.json();
-
-      console.log(resJson);
-
-      if(resJson.errors){
-        this.setState({errors: resJson.errors});
-      } else{
-        this.setState({loading: false});
-      }
-
-    } catch(error){
-      console.log(error);
-    }
+    });
   }
 
   async removeFile(e) {
@@ -62,7 +63,12 @@ export default class PostDropzone extends Component {
               <div className="col-30 align-self-center">
                 {
                   this.state.loading === true ?
-                  (<i className="fa fa-refresh fa-spin fa-2x text-info"></i>)
+                  (
+                    <div className="text-info">
+                      <i className="fa fa-refresh fa-spin fa-2x"></i>
+                      <small>{this.state.progress}</small>
+                    </div>
+                  )
                   : this.state.loading === false ?
                   (<i className="fa fa-check-square fa-2x text-success"></i>)
                   : null
