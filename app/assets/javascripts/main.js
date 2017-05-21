af4b8a67f7c8797cc6425d16fa5c5588b6e4b086
Guilherme Zordan <guilhermezordan@outlook.com>
2017-05-20 22:51:40 -0300

    novo layout mobile

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,22 +1,25 @@
 $(function(){
-  if($(".posts").length){
-    var lastScrollTop = 0;
-    $(".posts").scroll(function(event){
-      var st = $(".posts").scrollTop();
-
-      if (st > lastScrollTop){
-        if($(".header.sub").length){
-          $(".header.sub").css("display", "none");
-        }
-      } else{
-       if($(".header.sub").length){
-         $(".header.sub").css("display", "block");
-       }
-      }
-
-      lastScrollTop = st;
-    });
-  }
+  // if($(".posts").length){
+  //   var lastScrollTop = 0;
+  //   $(".posts").scroll(function(event){
+  //     var st = $(".posts").scrollTop();
+  //
+  //     if (st > lastScrollTop){
+  //       if($(".header.sub").length){
+  //         $(".header.sub").slideUp(400);
+  //       }
+  //     } else{
+  //      if($(".header.sub").length){
+  //        $(".header.sub").css("visibility", "visible");
+  //        setTimeout(function(){
+  //          $(".header.sub").slideDown(400);
+  //        }, 4000);
+  //      }
+  //     }
+  //
+  //     lastScrollTop = st;
+  //   });
+  // }
 
   if($(".communications").length){
     setTimeout(function(){
