a001e673e6f0b6eb2b205ddd00846f7a1c0dffea
Guilherme Zordan <guilhermezordan@outlook.com>
2017-05-22 15:36:55 -0300

    diminuicao footer post e usuario nao pode mais dar zoom no app

--- client/app/post/templates/PostBoxViewMobile.jsx
+++ client/app/post/templates/PostBoxViewMobile.jsx
@@ -29,20 +29,26 @@ export default class PostBoxViewMobile extends Component {
           postId={this.props.post.id} />
 
           <div className="row pr-2 pl-2">
-            <div className="col-8 mt-1">
+            <div className="col-12">
               <small className="text-muted">
-              {this.props.points || 0} {helper.pluralize(this.props.points, "ponto")}<br/>
-              Publicado { moment(this.props.post.createdAt).fromNow() }
+              {this.props.points || 0} {helper.pluralize(this.props.points, "ponto")}
+              &nbsp;&bull;&nbsp;Publicado { moment(this.props.post.createdAt).fromNow() }
               </small>
             </div>
-            <div className="col-4 mt-2 text-right align-self-center">
-              <button className="btn btn-block btn-primary p-2">
-                <i className="fa fa-comments"></i> 0
-              </button>
-            </div>
-            <div className="col-12 mt-3">
+          </div>
+
+          <div className="row pr-2 pl-2 mt-1">
+            <div className="col-9 pr-0">
               <PostVoteBox post={this.props.post} isMobile="true" />
             </div>
+            <div className="col-3 pl-0 text-right align-self-center">
+              <button className="btn btn-secondary p-2">
+                <i className="fa fa-comments"></i> 999
+              </button>
+            </div>
+          </div>
+
+          <div className="row pr-2 pl-2">
             <div className="col-12 mt-2">
               <PostShareLinks post={this.props.post} />
             </div>
