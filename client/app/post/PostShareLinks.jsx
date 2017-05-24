82602a9a0dea7b80f8bb9438acd0ab64a46214fe
Guilherme Zordan <guilhermezordan@outlook.com>
2017-05-24 12:50:40 -0300

    novo dialog sobre compartilhamento de post

--- client/app/post/PostShareLinks.jsx
+++ client/app/post/PostShareLinks.jsx
@@ -13,38 +13,76 @@ export default class PostShareLinks extends Component {
   }
 
   render(){
-    return (
-      <div className="row">
-        <div className="col-5 pr-0">
-          <CopyToClipboard
-            text={this.state.postUrl}
-            onCopy={()=> {
-              let copyBtn = $('[data-toggle='+this.props.post.id+']');
-              copyBtn.tooltip({trigger: 'manual'});
-              copyBtn.tooltip('show');
-              setTimeout(()=>{ copyBtn.tooltip('hide'); }, 3000);
-            }}>
-            <button className="btn btn-sm btn-block btn-success"
-            data-toggle={this.props.post.id}
-            data-placement="bottom" data-title="Copiado!">
-              <i className="fa fa-clipboard fa-1x"></i>
-              <span className="ml-1">Copiar Link</span>
+    if (this.props.isMobile){
+      return(
+        <div className="row mt-3">
+          <div className="col-12">
+            <CopyToClipboard
+              text={this.state.postUrl}
+              onCopy={()=> {
+                let copyBtn = $('[data-toggle='+this.props.post.id+']');
+                copyBtn.tooltip({trigger: 'manual'});
+                copyBtn.tooltip('show');
+                setTimeout(()=>{ copyBtn.tooltip('hide'); }, 3000);
+              }}>
+              <button className="btn btn btn-block btn-success"
+              data-toggle={this.props.post.id}
+              data-placement="bottom" data-title="Copiado!">
+                <i className="fa fa-clipboard fa-1x"></i>
+                <span className="ml-1">Copiar Link</span>
+              </button>
+            </CopyToClipboard>
+            <hr/>
+          </div>
+          <div className="col-12">
+            <button className="btn btn btn-block btn-facebook">
+              <i className="fa fa-facebook-f fa-1x"></i>
+              <span className="ml-1">Facebook</span>
             </button>
-          </CopyToClipboard>
+            <hr/>
+          </div>
+          <div className="col-12">
+            <button className="btn btn btn-block btn-twitter">
+              <i className="fa fa-twitter fa-1x"></i>
+              <span className="ml-1">Twitter</span>
+            </button>
+          </div>
         </div>
-        <div className="col-3 pl-1 pr-0">
-          <button className="btn btn-sm btn-block btn-facebook">
-            <i className="fa fa-facebook-f fa-1x"></i>
-            <span className="ml-1">Facebook</span>
-          </button>
+      );
+    } else {
+      return (
+        <div className="row">
+          <div className="col-4 pr-0">
+            <CopyToClipboard
+              text={this.state.postUrl}
+              onCopy={()=> {
+                let copyBtn = $('[data-toggle='+this.props.post.id+']');
+                copyBtn.tooltip({trigger: 'manual'});
+                copyBtn.tooltip('show');
+                setTimeout(()=>{ copyBtn.tooltip('hide'); }, 3000);
+              }}>
+              <button className="btn btn-sm btn-block btn-success"
+              data-toggle={this.props.post.id}
+              data-placement="bottom" data-title="Copiado!">
+                <i className="fa fa-clipboard fa-1x"></i>
+                <span className="ml-1">Copiar Link</span>
+              </button>
+            </CopyToClipboard>
+          </div>
+          <div className="col-4 pl-1 pr-0">
+            <button className="btn btn-sm btn-block btn-facebook">
+              <i className="fa fa-facebook-f fa-1x"></i>
+              <span className="ml-1">Facebook</span>
+            </button>
+          </div>
+          <div className="col-4 pl-1">
+            <button className="btn btn-sm btn-block btn-twitter">
+              <i className="fa fa-twitter fa-1x"></i>
+              <span className="ml-1">Twitter</span>
+            </button>
+          </div>
         </div>
-        <div className="col-4 pl-1">
-          <button className="btn btn-sm btn-block btn-twitter">
-            <i className="fa fa-twitter fa-1x"></i>
-            <span className="ml-1">Twitter</span>
-          </button>
-        </div>
-      </div>
-    );
+      );
+    }
   }
 }
--- client/app/post/templates/PostBoxViewMobile.jsx
+++ client/app/post/templates/PostBoxViewMobile.jsx
@@ -30,59 +30,74 @@ export default class PostBoxViewMobile extends Component {
 
   render(){
     return (
-      <div className="row justify-content-center mb-2 mt-2 no-gutters">
-        <div className="col-12 p-0">
-          <h1 className="pl-2 pr-2">
-            {this.props.post.title}
-          </h1>
-          { this.props.post.original ?
-            (
-              <h4>
-                <a href={"//"+this.props.post.original} target="_blank" content="noindex, nofollow">
-                  link do autor original
-                </a>
-              </h4>
-            )
-            : null }
+      <box>
+        <div className="row justify-content-center mb-2 mt-2 no-gutters">
+          <div className="col-12 p-0">
+            <h1 className="pl-2 pr-2">
+              {this.props.post.title}
+            </h1>
+            { this.props.post.original ?
+              (
+                <h4>
+                  <a href={"//"+this.props.post.original} target="_blank" content="noindex, nofollow">
+                    link do autor original
+                  </a>
+                </h4>
+              )
+              : null }
 
-          <PostMedia
-          image={this.props.post.image}
-          video={this.props.post.video}
-          postId={this.props.post.id} />
+            <PostMedia
+            image={this.props.post.image}
+            video={this.props.post.video}
+            postId={this.props.post.id} />
 
-          <div className="row pr-2 pl-2 mt-1">
-            <div className="col-2 align-self-center pr-0 text-center">
-              {this.props.post.commentCount || 0}
-              <button data-toggle="modal" data-target="#m_comments"
-              className="btn btn-block btn-secondary p-2">
-                <i className="fa fa-comments"></i>
-              </button>
+            <div className="row pr-2 pl-2 mt-1">
+              <div className="col-2 align-self-center pr-0 text-center">
+                {this.props.post.commentCount || 0}
+                <button data-toggle="modal" data-target="#m_comments"
+                className="btn btn-block btn-secondary p-2">
+                  <i className="fa fa-comments"></i>
+                </button>
+              </div>
+
+              <div className="col-8 text-center pr-1 pl-1">
+                <small className="text-muted">
+                  {this.props.points || 0} {helper.pluralize(this.props.points, "ponto")}
+                  &nbsp;&bull;&nbsp;publicado { moment(this.props.post.createdAt).fromNow() }
+                </small>
+                <PostVoteBox post={this.props.post} isMobile="true" />
+              </div>
+
+              <div className="col-2 align-self-center pl-0">
+                &nbsp;
+                <button data-toggle="modal" data-target="#m_share_links"
+                className="btn btn-block btn-secondary p-2">
+                  <i className="fa fa-share-alt"></i>
+                </button>
+              </div>
             </div>
 
-            <div className="col-8 text-center pr-1 pl-1">
-              <small className="text-muted">
-                {this.props.points || 0} {helper.pluralize(this.props.points, "ponto")}
-                &nbsp;&bull;&nbsp;publicado { moment(this.props.post.createdAt).fromNow() }
-              </small>
-              <PostVoteBox post={this.props.post} isMobile="true" />
-            </div>
-
-            <div className="col-2 align-self-center pl-0">
-              &nbsp;
-              <button onClick={()=> this.toggleShareLinks()}
-              className="btn btn-block btn-secondary p-2">
-                <i className="fa fa-share-alt"></i>
-              </button>
-            </div>
-            {
-              this.state.share === true ?
-              (<div className="col-12 mt-3"><PostShareLinks post={this.props.post} /></div>) : null
-            }
+            <hr className="mt-3" />
           </div>
 
-          <hr className="mt-3" />
         </div>
-      </div>
+        <div className="modal" id="m_share_links">
+          <div className="modal-dialog mobile-share-links">
+            <div className="row">
+              <div className="col-8 text-muted">
+                Compartilhar
+              </div>
+              <div className="col-4">
+                <button type="button" className="close" data-dismiss="modal">
+                  <span>&times;</span>
+                </button>
+              </div>
+            </div>
+
+            <PostShareLinks post={this.props.post} isMobile="true" />
+          </div>
+        </div>
+      </box>
     );
   }
 }
