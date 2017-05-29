2905e3611a79df1736ff4299e574978ac8107073
Guilherme Zordan <guilhermezordan@outlook.com>
2017-05-29 15:39:24 -0300

    separado layout devise mobile para desktop

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,10 +2,10 @@ $(function(){
   if($(".posts").length && $('.sub-header-mobile').length){
     $(".posts").scroll(function(event){
       var subHeader = $('.sub-header-mobile'),
-          scroll = $('.posts').scrollTop();
+      scroll = $('.posts').scrollTop();
 
       if (scroll >= 40) {
-        subHeader.addClass('fixed-top', 'mt');
+        subHeader.addClass('fixed-top');
       } else{
         subHeader.removeClass('fixed-top');
       }
