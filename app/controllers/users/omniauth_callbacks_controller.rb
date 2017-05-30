5dbbc9991caa9eec9cd473ba12d5d3b5d2897b12
Guilherme Zordan <guilhermezordan@outlook.com>
2017-05-30 14:50:04 -0300

    correcao bug ao criar conta via social

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -75,7 +75,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       render :index
     else
       session["omniauth_data"] = request.env["omniauth.auth"].except("extra")
-      render :index
       redirect_to new_user_registration_url
     end
   end
