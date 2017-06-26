03b2259b1b0936b415887d9020ad0189f42765fe
Guilherme Zordan <guilherme@triafu.com.br>
2017-06-25 22:53:27 -0300

    reconfirmacao de email

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -40,7 +40,7 @@ $(function(){
 
   if($(".communications").length){
     setTimeout(function(){
-      $(".communications").slideUp(300);
-    }, 4000);
+      $(".communications").slideUp(200);
+    }, 5000);
   }
 });
