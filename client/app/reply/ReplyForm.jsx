35239d87ecfcd5e739bd6a78a92bc82e4ab58d00
Guilherme Zordan <guilherme@triafu.com.br>
2017-07-08 20:59:09 -0300

    adicionada margem de form

--- client/app/reply/ReplyForm.jsx
+++ client/app/reply/ReplyForm.jsx
@@ -45,38 +45,38 @@ export default class ReplyForm extends Component {
 
   render(){
     return (
-      <form onSubmit={(e) => this.reply(e)} method="post" className="mb-3">
+      <form onSubmit={(e) => this.reply(e)} method="post" className="mb-3 mt-1">
 
-      <div className={"form-group mb-2" + (this.state.errors.hasOwnProperty('text') ? " has-danger" : "")}>
-        <TextAreaAutosize
-          onKeyUp={(e) => this.isSubmiting(e)}
-          value={this.state.text}
-          onChange={helper.handleChange.bind(this, 'text')}
-          style={{maxHeight: 50}}
-          autoFocus
-          placeholder="escreva um comentário" />
-      </div>
-
-      <div className="row">
-        <div className="col-6 pr-0">
-          <span className="text-muted mr-2">
-            {500 - this.state.text.length}
-          </span>
-
-          <ErrorMessage message={this.state.errors.text}/>
+        <div className={"form-group mb-2" + (this.state.errors.hasOwnProperty('text') ? " has-danger" : "")}>
+          <TextAreaAutosize
+            onKeyUp={(e) => this.isSubmiting(e)}
+            value={this.state.text}
+            onChange={helper.handleChange.bind(this, 'text')}
+            style={{maxHeight: 50}}
+            autoFocus
+            placeholder="escreva um comentário" />
         </div>
-        <div className="col-6 text-right pl-0">
 
-          <small>
-            <a href onClick={(e)=> {e.preventDefault(); pubsub.publish('toggle-reply-to', null)}}>
-              cancelar
-            </a>
-          </small>
+        <div className="row">
+          <div className="col-6 pr-0">
+            <span className="text-muted mr-2">
+              {500 - this.state.text.length}
+            </span>
 
-          <button type="submit" className="btn btn-sm btn-success ml-2"
-          disabled={this.state.loading}>Responder</button>
+            <ErrorMessage message={this.state.errors.text}/>
+          </div>
+          <div className="col-6 text-right pl-0">
+
+            <small>
+              <a href onClick={(e)=> {e.preventDefault(); pubsub.publish('toggle-reply-to', null)}}>
+                cancelar
+              </a>
+            </small>
+
+            <button type="submit" className="btn btn-sm btn-success ml-2"
+            disabled={this.state.loading}>Responder</button>
+          </div>
         </div>
-      </div>
 
       </form>
     );
